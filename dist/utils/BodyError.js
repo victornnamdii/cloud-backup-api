cb2a3b2c54e742219eecd60de2d971976b3151c8
“victornnamdii” <victorilodiuba@gmail.com>
2023-08-30 12:13:04 +0100

    remove dist

--- dist/utils/BodyError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class RequestError extends Error {
-    /* eslint-disable-next-line @typescript-eslint/no-useless-constructor */
-    constructor(message) {
-        super(message);
-    }
-}
-exports.default = RequestError;
--- dist/utils/generatePassword.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const crypto_1 = require("crypto");
-const generatePassword = () => {
-    const buffer = (0, crypto_1.randomBytes)(8);
-    return buffer.toString('base64');
-};
-exports.default = generatePassword;
